c60440e97f1010d031aa1cc0f391f19a25981615
Willis Plummer <willisplummer@gmail.com>
2023-08-08 10:31:23 -0600

    Initial Commit

---
+++ snake/snakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  snakeApp.swift
+//  snake
+//
+//  Created by Willis Plummer on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct snakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
